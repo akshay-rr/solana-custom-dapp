76f99a731f2a36d1cfff5b1147d9377053423a08
akshay-rr <akshayrajagopal10@gmail.com>
2022-09-16 19:47:57 +0530

    solana sample dapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [account, setAccount] = useState(window.solana2);
+
+    useEffect(() => {
+        setAccount(window.solana2);
+    }, []);
+
+    const Connect = () => {
+        return (
+            <button 
+                className='btn btn-primary'
+                onClick={() => {
+                    window.solana2.connect();
+                }}>
+                Connect
+            </button>
+        )
+    }
+
+    const ConnectedApp = () => {
+        return (
+            <div>
+                {window.solana2.account}
+            </div>
+        )
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                {
+                    (window.solana2 != null) ?
+                    ((window.solana2.account) ?
+                    <ConnectedApp /> :
+                    <Connect />) :
+                    <div>
+                        <button 
+                            className='btn btn-primary'
+                            onClick={() => {
+                                setAccount(window.solana2)
+                            }}>
+                            Refresh Window Variable
+                        </button>
+                    </div>
+                }
+            </header>
+        </div>
+    );
 }
 
 export default App;
